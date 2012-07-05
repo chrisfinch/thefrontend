e0658e22bcf0db44beaaaa9c6a64379250b5d4ab
Ben Constable <ben@benconstable.co.uk>
2012-07-05 23:54:21 +0100

    Added Update and Show methods for Jobs, bit of tidying up

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,31 +2,59 @@ class JobsController < ApplicationController
 
   respond_to :json
 
+  # Show all Jobs
   def index
+
     respond_with Job.all
+
   end
 
+  # Show the form to create new jobs
   def new
   end
 
+  # Create a new Job in the database
   def create
-    respond_with Job.create(
-      title: params[:title],
-      description: params[:description],
-      keywords: params[:keywords]
-    )
-  end
 
+    createFields = {}
+
+    Job.relevantFields.each do |field|
+      createFields[field] = params[field.to_sym]
+    end
+
+    respond_with Job.create(createFields)
+
+  end
+  
+  # Show a single Job
   def show
+
+    respond_with Job.find(params[:id])
+
   end
 
+  # Show the edit form for a Job
   def edit
   end
 
+  # Update a Job in the database
   def update
+
+    job = Job.find(params[:id])
+
+    Job.relevantFields.each do |field|
+      job[field.to_sym] = params[field.to_sym]
+    end
+
+    respond_with job.save
+
   end
 
+  # Remove a Job from the database
   def destroy
+
+    respond_with Job.find(params[:id]).delete
+
   end
   
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -5,4 +5,15 @@ class Job
   field :description, type: String
   field :keywords, type: String
 
+  # Get an array of the field names on this Document. Does not include _id.
+  def self.storageFields
+
+    keys = Job.fields.keys
+    keys.delete('_id')
+    keys.delete('_type')
+    
+    return keys
+
+  end
+
 end
