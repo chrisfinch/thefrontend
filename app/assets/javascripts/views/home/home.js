68c736b1b57c4ce62621b21bfb4cf74c2c4fedb1
Chris Finch <chris@overninethousand.co.uk>
2012-07-18 22:59:12 +0100

    Bound events to home search box for default value replacement and submitting

--- app/assets/javascripts/views/home/home.js
+++ app/assets/javascripts/views/home/home.js
@@ -2,11 +2,37 @@ Thefrontend.Views.home = Backbone.View.extend({
 	
 	template: JST['home/home'],
 
+	events: {
+		"focus #search"	: "searchFieldEvents",
+		"blur #search"	: "searchFieldEvents",		
+		"keyup #search"	: "searchFieldEvents"
+	},
+
 	/*
 	 * Initialize with the template-id
 	 */
 	initialize: function() {
-	
+		// nada..
+	},
+
+	searchFieldEvents: function (event) {
+		var $e = $(event.target);
+		switch (event.type) {
+			case 'focusin':
+				$e.val('');
+				break;
+			case 'focusout':
+				if ($e.val() == '') {
+					$e.val($e.data('val'));
+				}
+				break;	
+			case 'keyup':
+				if (event.keyCode == 13) {
+					console.log('enter key pressed.');
+					// submit!
+				}
+				break;
+		}
 	},
 
 	/*
