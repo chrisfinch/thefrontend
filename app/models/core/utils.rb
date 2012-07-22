6f407e059af7b48ea88e50f9dc6633474b4bdd18
Ben Constable <ben@benconstable.co.uk>
2012-07-22 22:15:44 +0100

    Removed some unused files

--- app/models/core/utils.rb
+++
@@ -1,9 +0,0 @@
-class Utils
-
-  # Generate a random string of the given length (lcase, upcase and digits)
-  def self.random_string(length)
-    rands = ('A'..'Z').to_a + ('a'..'z').to_a + (0..9).to_a
-    (0..length).map { rands[rand(rands.length)] }.join
-  end
-
-end
