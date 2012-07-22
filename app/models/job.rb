e5da80cded899e2ca99da14ef702491e13c883c1
Ben Constable <ben@benconstable.co.uk>
2012-07-22 22:14:09 +0100

    Remove keywords from Jobs and move common mixins to lib/

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,24 +1,14 @@
+# Job entity
 class Job
 
   include Mongoid::Document
-  
+  include Thefrontend::Mongo
+
   field :title, type: String
   field :company, type: String
   field :salary, type: String
   field :description, type: String
   field :type, type: String
-  field :keywords, type: Array
   field :tags, type: Array
-
-  # Get an array of the field names on this Document. Does not include _id.
-  def self.storage_fields
-
-    keys = Job.fields.keys
-    keys.delete('_id')
-    keys.delete('_type')
-    
-    return keys
-
-  end
 
 end
