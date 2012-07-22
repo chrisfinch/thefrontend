a1a94046735cbc2f70ef08723eec6f21dc06d6b5
Chris Finch <chris@overninethousand.co.uk>
2012-07-22 01:57:31 +0100

    Styled up signin form and added appear keyframe animation and basic text field handling events

--- app/assets/javascripts/views/user/sign_in.js
+++ app/assets/javascripts/views/user/sign_in.js
@@ -2,15 +2,60 @@ Thefrontend.Views.sign_in = Backbone.View.extend({
 
   template: JST['user/sign_in'],
   
-  // events: {
-  //   'submit #new-job': 'createJob',
-  //   'click .remove-job': 'removeJob'
-  // },
+  events: {
+    'click .signInButton': 'signIn',
+    'focus #username': 'fieldsUsername',
+    'focus #password': 'fieldsPassword',
+    'blur #username': 'fieldsUsername',
+    'blur #password': 'fieldsPassword',    
+  },
   
   initialize: function() {
     //this.collection.on('reset', this.render, this);
   },
   
+  signIn: function () {
+    this.$el.find('#signInForm')
+    .show()
+    .find('#username')
+    .focus()
+    .val(
+      this.$el.find('#username').val()
+    );
+  },
+
+  fieldsUsername: function (event) {
+    var $e = $(event.target);
+    switch (event.type) {
+      case 'focusin':
+        if ($e.val() == $e.data('val')) {
+          $e.val('');
+        }
+        break;
+      case 'focusout':
+        if ($e.val() == '') {
+          $e.val($e.data('val'));
+        }
+        break;
+    }      
+  },
+
+  fieldsPassword: function (event) {
+    var $e = $(event.target);
+    switch (event.type) {
+      case 'focusin':
+        if ($e.val() == $e.data('val')) {        
+          $e.val('')[0].type = 'password';
+        }
+        break;
+      case 'focusout':
+        if ($e.val() == '') {
+          $e.val($e.data('val'))[0].type = 'text';
+        }
+        break;
+    }      
+  },  
+
   render: function() {
     $(this.el).html(this.template());
     return this;
